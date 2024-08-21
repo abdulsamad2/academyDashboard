9ec6bd23fc7b6e6f341b6014461185b409047475
Abdul Samad <abdulsamad.laghari1@gmail.com>
2024-08-22 01:09:41 +0500

    Update student-form.tsx

--- components/forms/student-form.tsx
+++ components/forms/student-form.tsx
@@ -19,7 +19,7 @@ import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { useToast } from '../ui/use-toast';
 import { Textarea } from '../ui/textarea';
-import { tutorRegistration } from '@/action/tutorRegistration';
+import { StudentRegistration } from '@/action/StudentRegistration';
 import { AlertModal } from '../modal/alert-modal';
 import InputformField from '../formField';
 import SelectFormField from '../selectFromField';
@@ -70,6 +70,12 @@ const MStates = [
   { label: 'Sabah', value: 'sb' },
   { label: 'Sarawak', value: 'srw' }
 ] as const;
+const Gender = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+  { label: 'other', value: 'other' }
+  
+] as const;
 const teachOnline = [
   { label: 'Yes', value: 'true' },
   { label: 'No', value: 'false' }
@@ -77,11 +83,10 @@ const teachOnline = [
 
 
 const FormSchema = z.object({
-  bio: z.string().min(1, { message: 'Bio must be at least 50 character' }),
   email: z.string().email({ message: 'Enter a valid email address' }),
   name: z
     .string()
-    .min(3, { message: 'Tutor Name must be at least 3 characters' }),
+    .min(3, { message: 'Student Name must be at least 3 characters' }),
   state: z.string().min(1, { message: 'Please select a state' }),
   password: z.string(),
   phone: z
@@ -91,62 +96,34 @@ const FormSchema = z.object({
     .string()
     .min(1, { message: 'Address must be at least 1 character' }),
   city: z.string().min(1, { message: 'City must be at least 1 character' }),
-  bank: z
-    .string()
-    .min(1, { message: 'add bank name and should be more than one' }),
-  bankaccount: z
-    .string()
-    .min(1, { message: 'Bank must be at least 8 character' }),
-  currentposition: z.string().min(1, {
-    message: 'Current workin position must be at least 1 character'
-  }),
-  education: z
-    .string()
-    .min(1, { message: 'Education must be at least 1 character' }),
-  certification: z
-    .string()
-    .min(1, { message: 'Certification must be at least 1 character' }),
-  subjects: z
-    .string()
-    .min(1, { message: 'Please select at least one subject' }),
   online: z.string(),
-  experience: z
-    .string()
-    .min(1, { message: 'Experince must be at least 50 character' }),
-  profilepic: z.string().min(1, { message: 'Profile image must be uploaded' }),
-  nric: z.string().min(1, { message: 'nric must be uploaded' }),
-  stt: z.string().min(1, { message: 'stt must be uploaded' }),
-  resume: z.string().min(1, { message: 'resume must be uploaded' }),
+  gender:z.string(),
 
-
-
-
+  profilepic: z.string().min(1, { message: 'Profile image must be uploaded' })
 
 });
 
-type TutorFormValues = z.infer<typeof FormSchema>;
+type studentFormValue = z.infer<typeof FormSchema>;
 
-interface TutorFormProps {
-  initialData: TutorFormValues | null;
+interface StudentFormProps {
+  initialData: studentFormValue | null;
 }
 
-export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
+export const StudentForm: React.FC<StudentFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
   const { toast } = useToast();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const title = initialData ? 'Edit tutor' : 'Create tutor';
-  const description = initialData ? 'Edit a tutor.' : 'Add a new tutor';
-  const toastMessage = initialData ? 'Tutor updated.' : 'Tutor created.';
+  const title = initialData ? 'Edit Student' : 'Create Student';
+  const description = initialData ? 'Edit a Student.' : 'Add a new Student';
+  const toastMessage = initialData ? 'Student updated.' : 'Student created.';
   const action = initialData ? 'Save changes' : 'Create';
 
   const defaultValues = initialData
     ? initialData
     : {
-      bio: '',
-      experience: '',
       name: '',
       email: '',
       password: '',
@@ -154,26 +131,18 @@ export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
       state: '',
       addess: '',
       city: '',
-      bank: '',
-      bankaccount: '',
-      currentposition: '',
-      education: '',
-      certification: '',
-      subjects: '',
       online: false,
       profilepic: '',
-      nric: '',
-      stt: '',
-      resume: '',
+
     };
 
     console.log('inital data =>',initialData?.profilepic)
-  const form = useForm<TutorFormValues>({
+  const form = useForm<studentFormValue>({
     resolver: zodResolver(FormSchema),
     defaultValues
   });
 
-  const onSubmit = async (data: TutorFormValues) => {
+  const onSubmit = async (data: studentFormValue) => {
 
     // console.log(fData)
     // // const data = new FormData();
@@ -192,7 +161,7 @@ export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
 
     try {
       setLoading(true);
-      const res = await tutorRegistration(data);
+      // const res = await StudentRegistration(data);
       if (res.error) {
         toast({
           variant: 'destructive',
@@ -204,10 +173,10 @@ export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
         toast({
           variant: 'default',
           title: toastMessage,
-          description: 'Tutor details updated successfully'
+          description: 'Student details updated successfully'
         });
         // router.refresh();
-        // router.push(`/dashboard/tutors/${res._id}`);
+        // router.push(`/dashboard/Students/${res._id}`);
       } else {
         toast({
           variant: 'destructive',
@@ -268,50 +237,13 @@ export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
             )}
           </div>
           <Separator />
-          <FormField
-            className="w-full"
-            control={form.control}
-            name="bio"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Add your Bio</FormLabel>
-                <FormControl>
-                  <Textarea
-                    disabled={loading}
-                    placeholder="Tell us about yourself journy education intestest."
-                    rows={4}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            className="w-full"
-            control={form.control}
-            name="experience"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Your Teaching Experince</FormLabel>
-                <FormControl>
-                  <Textarea
-                    disabled={loading}
-                    placeholder="Tell us about your teaching experience what makes you a good teacher."
-                    rows={4}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        
           <div className="gap-8 md:grid md:grid-cols-3">
             <InputformField
               control={form.control}
               loading={loading}
               label={'Name'}
-              placeholder={'add your Name'}
+              placeholder={'Jameel'}
               type={'text'}
               name={'name'}
             />
@@ -319,7 +251,7 @@ export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
               control={form.control}
               loading={loading}
               label={'Email'}
-              placeholder={'add your Email'}
+              placeholder={'exmaple@gmail.com'}
               type={'email'}
               name={'email'}
             />
@@ -343,21 +275,11 @@ export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
               control={form.control}
               loading={loading}
               label={'Address'}
-              placeholder={'Address must be 8 character long'}
+              placeholder={'kaulalampur 2nd street'}
               type={'text'}
               name={'address'}
             />
-            <SelectFormField
-              control={form.control}
-              loading={loading}
-              label={'State you can teach'}
-              placeholder={'Select state a state'}
-              name={'state'}
-              form={form}
-              options={MStates}
-            />
-
-            <InputformField
+              <InputformField
               control={form.control}
               loading={loading}
               label={'City'}
@@ -365,117 +287,27 @@ export const StudentForm: React.FC<TutorFormProps> = ({ initialData }) => {
               type={'text'}
               name={'city'}
             />
-            <InputformField
+            <SelectFormField
               control={form.control}
               loading={loading}
-              label={'Bank'}
-              placeholder={'Bank Name'}
-              type={'text'}
-              name={'bank'}
-            />
-            <InputformField
-              control={form.control}
-              loading={loading}
-              label={'Bank Account #'}
-              placeholder={'add your bank account #'}
-              type={'text'}
-              name={'bankaccount'}
-            />
-            <InputformField
-              control={form.control}
-              loading={loading}
-              label={'Current working Position'}
-              placeholder={'Marketing Manager at UHIL'}
-              type={'text'}
-              name={'currentposition'}
-            />
-            <InputformField
-              control={form.control}
-              loading={loading}
-              label={'Your Education Level'}
-              placeholder={'Masters in Information Technology from MIT'}
-              type={'text'}
-              name={'education'}
-            />
-            <InputformField
-              control={form.control}
-              loading={loading}
-              label={'Name of highest certificate'}
-              placeholder={'Eg: Effective online teaching from coursera'}
-              type={'text'}
-              name={'certification'}
-            />
-            <InputformField
-              control={form.control}
-              loading={loading}
-              label={'Subjects I can teach'}
-              placeholder={'add subject name sepperated by commma'}
-              type={'text'}
-              name={'subjects'}
+              label={'State'}
+              placeholder={'Select gender'}
+              name={'state'}
+              form={form}
+              options={MStates}
             />
             <SelectFormField
               control={form.control}
               loading={loading}
-              label={'Can Teach Online'}
-              placeholder={'I want to teach online'}
-              name={'online'}
+              label={'Gender'}
+              placeholder={'Select gender'}
+              name={'gender'}
               form={form}
-              options={teachOnline}
-            />
-          </div>
-          <Separator />
-          <h2 className="py-4 text-center text-xl">Upload your Documents</h2>
-          <div className="gap-8 md:grid md:grid-cols-3">
-            <FormField
-              control={form.control}
-              name="profilepic"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <CloudinaryUpload title={'Upload Profile Picture'} initialUrl={initialData?.profilepic} onUpload={(url) => field.onChange(url)} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="nric"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <CloudinaryUpload title={'Upload nric'}  initialUrl={initialData?.nric} onUpload={(url) => field.onChange(url)} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+              options={Gender}
             />
 
-            <FormField
-              control={form.control}
-              name="stt"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <CloudinaryUpload title={'Upload stt'} onUpload={(url) => field.onChange(url)} initialUrl={initialData?.stt}  />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="resume"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <CloudinaryUpload title={'Upload resume'} onUpload={(url) => field.onChange(url)} initialUrl={initialData?.resume} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
           </div>
+  
 
 
           <Button className="justify-center w-1/4 mt-6" type="submit">
