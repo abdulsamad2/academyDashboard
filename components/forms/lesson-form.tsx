1c477a6f745a2261ababab14b27058375f843319
Abdul Samad <abdulsamad.laghari1@gmail.com>
2025-05-01 20:27:13 +0500

    Update lesson-form.tsx

--- components/forms/lesson-form.tsx
+++ components/forms/lesson-form.tsx
@@ -253,7 +253,7 @@ export const LessonForm: React.FC<LessonFormProps> = ({ initialData }) => {
                   label={'Date *'}
                   placeholder={'Select Date'}
                   control={form.control}
-                  isAdmin={isAdmin}
+                  isAdmin={true}
                   className="w-full"
                 />
               </div>
